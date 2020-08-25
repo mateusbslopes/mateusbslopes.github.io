11b4fc6d1fb97beb91f689d61f7130ebb65a81f2
Mateus Lopes <mateusbslopes@gmail.com>
2020-08-24 21:28:57 -0300

    feat: Add theming

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import React from "react";
-import { Provider } from "react-redux";
 import Home from "./components/pages/Home";
-import store from "./store/ducks";
+import { Global, css } from "@emotion/core";
+import { connect, ConnectedProps } from "react-redux";
+import { ThemeProvider } from "emotion-theming";
+import themes from "./themes";
 
-function App() {
+function App({ theme }: AppProps) {
   return (
-    <div className="App">
-      <Provider store={store}>
-        <Home />
-      </Provider>
-    </div>
+    <ThemeProvider theme={(themes as any)[theme]}>
+      <Home />
+    </ThemeProvider>
   );
 }
 
-export default App;
+const mapStateToProps = (state: any) => ({
+  theme: state.theme.name,
+});
+
+const connector = connect(mapStateToProps);
+type AppProps = ConnectedProps<typeof connector>;
+
+export default connector(App);
