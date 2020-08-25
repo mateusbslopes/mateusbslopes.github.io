0584968d49d897331b9aa7fee8d5d48263d62021
Mateus Lopes <mateusbslopes@gmail.com>
2020-08-24 23:51:01 -0300

    Updates

---
+++ precache-manifest.76e75a5b6c47608260c816b492d45459.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d24406890d2d7252b5dfff36c0bfd78d",
+    "url": "/mateusbslopes.github.com/index.html"
+  },
+  {
+    "revision": "ef0309c738690f3e2fb5",
+    "url": "/mateusbslopes.github.com/static/js/2.a478d1a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mateusbslopes.github.com/static/js/2.a478d1a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "767bb7a78d8ed1d65a91",
+    "url": "/mateusbslopes.github.com/static/js/main.faca50b2.chunk.js"
+  },
+  {
+    "revision": "3f84e66c89fd84e4e536",
+    "url": "/mateusbslopes.github.com/static/js/runtime-main.665ec361.js"
+  }
+]);
