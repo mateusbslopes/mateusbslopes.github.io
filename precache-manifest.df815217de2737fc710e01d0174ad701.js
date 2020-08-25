5f6941688507c0a9246db87c58aaadf8681504b6
Mateus Lopes <mateusbslopes@gmail.com>
2020-08-24 23:52:57 -0300

    Updates

---
+++ precache-manifest.df815217de2737fc710e01d0174ad701.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3e468ccbb21d318b0c257f886e2e873",
+    "url": "/mateusbslopes.github.io/index.html"
+  },
+  {
+    "revision": "ef0309c738690f3e2fb5",
+    "url": "/mateusbslopes.github.io/static/js/2.a478d1a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mateusbslopes.github.io/static/js/2.a478d1a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b23d28f51399b3795b8b",
+    "url": "/mateusbslopes.github.io/static/js/main.ab3e3de2.chunk.js"
+  },
+  {
+    "revision": "5eaec9aecfcea8180c0f",
+    "url": "/mateusbslopes.github.io/static/js/runtime-main.004d996b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mateusbslopes.github.com/precache-manifest.76e75a5b6c47608260c816b492d45459.js"
+  "/mateusbslopes.github.io/precache-manifest.df815217de2737fc710e01d0174ad701.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mateusbslopes.github.com/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mateusbslopes.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
